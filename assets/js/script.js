b7c0d32a10c4c1ea4367f078c69ff913754960d4
adhilameenet <adhilameenet@gmail.com>
2025-03-28 21:20:47 +0530

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     VANTA.TOPOLOGY({
-//         el: "#hero",
-//         mouseControls: true,
-//         touchControls: true,
-//         gyroControls: false,
-//         minHeight: 200.00,
-//         minWidth: 200.00,
-//         scale: 1.00,
-//         scaleMobile: 1.00
-//     });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(
+    "[data-collapse-toggle='navbar-sticky']"
+  );
+  const menu = document.getElementById("navbar-sticky");
+  const menuItems = menu.querySelectorAll("a");
+
+  // Function to toggle menu visibility
+  function toggleMenu() {
+    menu.classList.toggle("hidden");
+  }
+
+  // Toggle menu when button is clicked
+  menuToggle.addEventListener("click", toggleMenu);
+
+  // Close menu when an item is clicked
+  menuItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      if (!menu.classList.contains("hidden")) {
+        toggleMenu();
+      }
+    });
+  });
+});
