9b343c82d54cb1bc1916135617d8eaf0c013a6a8
adhilameenet <adhilameenet@gmail.com>
2025-04-17 15:49:04 +0530

    about1

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
     
 
       
-      <section class="pt-10 bg-gray-100 sm:pt-16 lg:pt-24">
+      <section class="pt-10 sm:pt-16 lg:pt-24">
         <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
             <div class="max-w-2xl mx-auto text-center">
                 <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl lg:leading-tight">About the Event</h2>
