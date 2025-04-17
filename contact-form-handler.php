84fd6472ddd2a3b61a09aefe93961787897a5047
adhilameenet <adhilameenet@gmail.com>
2025-04-17 17:59:05 +0530

    fix

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,26 +1,6 @@
 <?php
 require_once 'config.php';
 
-
-
-// Prepare and bind
-$stmt = $conn->prepare("INSERT INTO form_submission (name, email, phone, company, message) VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param("sssss", $name, $email, $phone, $company, $message);
-
-// Execute and check
-if ($stmt->execute()) {
-    echo "<script>
-        alert('✅ Message sent successfully! We’ll get back to you soon.');
-        window.location.href = '/';
-    </script>";
-} else {
-    echo "<script>
-        alert('❌ Something went wrong. Please try again.');
-        window.history.back();
-    </script>";
-}
-
-
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
