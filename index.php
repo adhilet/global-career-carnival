e85e67dfbcabe80022399e4ffdb8b200569e1ab2
adhilameenet <adhilameenet@gmail.com>
2025-04-20 15:38:10 +0530

    fix nav logo

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="./assets/images/nav-logo.png" class="w-24" alt="Logo">
+                <img src="./assets/images/nav-logo-black.png" class="h-12 w-auto" alt="Logo">
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                 <button type="button"
@@ -82,7 +82,7 @@
         </div>
     </section> -->
 
-    <section class="py-20 sm:py-16 lg:py-2">
+    <section class="py-20 sm:py-16 lg:py-0">
         <div class="w-full">
             <!-- Mobile Banner -->
             <img src="./assets/images/banner-mobile.png" alt="Mobile Banner" class="block lg:hidden w-full h-auto">
